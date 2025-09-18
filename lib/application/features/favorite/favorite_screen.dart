78fb5fb6a9c7737e6ccc1c2f5d0fdf7cb3c457b8
sahad-systech <sahad@systech.ae>
2025-09-18 15:53:36 +0530

    add favorites

--- lib/application/features/favorite/favorite_screen.dart
+++ lib/application/features/favorite/favorite_screen.dart
@@ -1,11 +1,14 @@
-import 'dart:convert';
+import 'dart:math';
+
 import 'package:e_commerce/Widgets/favorite_card.dart';
 import 'package:e_commerce/application/core/widgets/appbar.dart';
-import 'package:e_commerce/data_base/function/favorite_function.dart';
-import 'package:e_commerce/data_base/models/favorite/favorite_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:lottie/lottie.dart';
+
+import '../../../data_base/models/favorite/favorite_model.dart';
+import '../../../service/favorite.dart';
+import '../../../service/model/favorite_model.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -17,64 +20,90 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    favoritee.getAllFavorite();
+    // favoritee.getAllFavorite();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: mainTitle('Favorite'),
-      body: ValueListenableBuilder(
-        valueListenable: favoriteNotifier,
-        builder: (BuildContext context, List<FavoriteModel> favoriteList,
-            Widget? child) {
-          if (favoriteList.isEmpty) {
-            return  Center(child: Lottie.asset("asset/Animation - 1717653689444.json",height: 250));
-          }
-          return ListView.builder(
-            itemCount: favoriteList.length,
-            itemBuilder: (context, index) {
-              final data = favoriteList[index];
-              final base64Image = data.image;
-              final imagebytes = base64.decode(base64Image);
-              GlobalKey<FormState> formkey = GlobalKey<FormState>();
-              return Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: SizedBox(
-                  height: 130,
-                  child: ValueListenableBuilder(
-                    valueListenable:
-                        Hive.box<FavoriteModel>('favorite_db').listenable(),
-                    builder: (context, box, child) {
-                      return Dismissible(
-                        key: formkey,
-                        onDismissed: (direction) async {
-                          setState(() {
-                            favoriteList.removeAt(index);
-                          });
-                          await box.delete(data.id);
-                          // favoritee.deleteFavorite(data.id!);
-                        },
-                        direction: DismissDirection.endToStart,
-                        background: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFFFE6E6),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.only(left: 280),
-                            child: Icon(
-                              Icons.delete,
-                              color: Color.fromARGB(255, 248, 55, 41),
+      floatingActionButton: IconButton(
+          onPressed: () {
+            int id = Random().nextInt(1000);
+            FavoriteApiService().addFavorite(Favorite(
+                id: id,
+                title: "Test $id",
+                price: 1000,
+                image:
+                    'https://images.unsplash.com/photo-1541643600914-78b084683601?q=80&w=808&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'));
+            setState(() {});
+          },
+          icon: const Icon(
+            Icons.heart_broken,
+          )),
+      body: FutureBuilder(
+        future: FavoriteApiService().getAllFavorites(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            final List<Favorite> favoriteList = snapshot.data;
+            if (favoriteList.isEmpty) {
+              return Center(
+                  child: Lottie.asset("asset/Animation - 1717653689444.json",
+                      height: 250));
+            }
+            return ListView.builder(
+              itemCount: favoriteList.length,
+              itemBuilder: (context, index) {
+                final data = favoriteList[index];
+                final image = data.image;
+
+                GlobalKey<FormState> formkey = GlobalKey<FormState>();
+                return Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: SizedBox(
+                    height: 130,
+                    child: ValueListenableBuilder(
+                      valueListenable:
+                          Hive.box<FavoriteModel>('favorite_db').listenable(),
+                      builder: (context, box, child) {
+                        return Dismissible(
+                          key: formkey,
+                          onDismissed: (direction) async {
+                            setState(() {
+                              favoriteList.removeAt(index);
+                            });
+                            await box.delete(data.id);
+                            // favoritee.deleteFavorite(data.id!);
+                          },
+                          direction: DismissDirection.endToStart,
+                          background: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFFFE6E6),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 280),
+                              child: Icon(
+                                Icons.delete,
+                                color: Color.fromARGB(255, 248, 55, 41),
+                              ),
                             ),
                           ),
-                        ),
-                        child: FavoriteCard(
-                            imagebytes: imagebytes, data: data, index: index),
-                      );
-                    },
+                          child: FavoriteCard(
+                              image: image, data: data, index: index),
+                        );
+                      },
+                    ),
                   ),
-                ),
-              );
-            },
-          );
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         },
       ),
     );
